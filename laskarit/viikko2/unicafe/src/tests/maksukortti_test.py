89881225424345294530bb480e2356071d54264d
Rasmus Paltschik <rjpalt@gmail.com>
2022-11-01 14:38:28 +0200

    Coverage

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,27 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_kortille_voi_ladata_rahaa(self):
+        self.maksukortti.lataa_rahaa(2500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 35.00 euroa")
+
+    def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
+        self.maksukortti.ota_rahaa(250)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 7.50 euroa")
+
+    def test_saldoylitys_ei_ota_rahaa(self):
+        self.maksukortti.ota_rahaa(1100)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_palauta_true_jos_saldo_riittaa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(100), True)
+
+    def test_palauta_false_jos_saldo_ei_riita(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1100), False)
