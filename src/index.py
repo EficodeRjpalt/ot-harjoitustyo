eb1f397504772145dfaa28e0a180e9acbb074685
Rasmus Paltschik <rjpalt@gmail.com>
2022-11-04 07:58:28 +0200

    Add stuff

--- src/index.py
+++ src/index.py
@@ -5,7 +5,7 @@ def main():
     config = configparser.ConfigParser()
     config.read('config.cfg')
 
-    print(config['DEFAULT']['test'])
+    print(config['mapping']['title'])
 
 if __name__ == "__main__":
     main()
--- src/tests/issue_test.py
+++ src/tests/issue_test.py
@@ -3,7 +3,7 @@ from issue import Issue
 
 class TestIssue(unittest.TestCase):
     def setUp(self):
-        issue = Issue(
+        self.issue = Issue(
             'Test title',
             'This issue is meant to test the Issue class',
             'Open',
@@ -13,3 +13,15 @@ class TestIssue(unittest.TestCase):
             '[2022-07-25T05:23:02Z]',
             ['tests', 'mock']
         )
+
+    def test_issue_attributes(self):
+
+        self.assertEqual(self.issue.title, 'Test title')
+        self.assertEqual(self.issue.description, 'This issue is meant to test the Issue class')
+        self.assertEqual(self.issue.state, 'Open')
+        self.assertEqual(self.issue.assignee, 'Pormestari Kontiainen')
+        self.assertEqual(self.issue.author, 'Herra Sitti-Sonniainen')
+        self.assertEqual(self.issue.created, '[2022-07-20T03:45:02Z]')
+        self.assertEqual(self.issue.closed, '[2022-07-25T05:23:02Z]')
+        self.assertListEqual(self.issue.labels, ['tests', 'mock'])
+        self.assertListEqual(self.issue.comments, [])
