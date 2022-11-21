e8a6e2b22981f7b380de8eee62f3442874d8ee49
Rasmus Paltschik <rjpalt@gmail.com>
2022-11-21 05:39:19 +0200

    Index.py toimintakuntoon

--- src/index.py
+++ src/index.py
@@ -12,20 +12,8 @@ def main():
 
     lukija = CSVReader(mapping_filepath, input_filepath)
 
-    dicts = lukija.read_csv_to_dict()
-    lista = lukija.transform_dict_items_into_issues(dicts)
-    for item in lista:
-        item.displaynames_to_emails('eficode.com')
-        item.timestamps_to_jira()
-        print(item.issue_to_dict())
+    lukija.transform_export_csv_to_import_csv()
 
 
 if __name__ == "__main__":
     main()
-
-####
-# 2 Do
-# - Testit koko tähän mennessä luodulle koodille
-# - CSV:n kirjoittaminen:
-# https://stackoverflow.com/questions/57097257/whats-the-best-way-to-unit-test-functions-that-handle-csv-files
-# - Docstringit
